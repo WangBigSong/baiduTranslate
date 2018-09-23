6a973a5975dd7877c1457b7259bafcfbe50f4cfe
XJwdk <42967864+XJwdk@users.noreply.github.com>
2018-09-24 00:58:38 +0800

    Update baiduTranslate.py
    Made some changes to fit my project

--- baiduTranslate.py
+++ baiduTranslate.py
@@ -1,83 +1,63 @@
-import urllib.request as request #请求模块
-import urllib.parse as parse #格式化请求参数模块
-import json #json模块
-import re #正则模块
-import execjs #运行本地js代码
+#encoding: utf - 8
 
-sign = token = set_cookies =  None
-#查询类型
-def _findType(query):
-    url = 'http://fanyi.baidu.com/langdetect'
-    data = {}
-    data["query"] = query
-    data = parse.urlencode(data).encode("utf-8")
-    result = None
-    with request.urlopen(url,data) as response:
-        result = response.read().decode("utf-8")
-    jsonResult = json.loads(result)
-    return jsonResult.get("lan")
+import urllib2 # 请求模块
+import urllib  # 格式化请求参数模块
+import json  # json模块
+import re  # 正则模块
+import execjs  # 运行本地js代码
 
-#查询翻译结果
-def findTranslateResult(query):
-    global sign,token,set_cookies
-    if set_cookies == None or sign == None:
-        with request.urlopen("http://fanyi.baidu.com/?aldtype=16047") as gtkResponse:
-            set_cookies = gtkResponse.info().get_all("Set-Cookie")
-            infoStr = gtkResponse.read().decode("utf-8")
-            gtkRe = re.search(r";window.gtk = '([\d\.]+)';",infoStr)
-            gtk = gtkRe.group(1)
-            sign = _calcSign(query,gtk)
-    cookie = ""
-    for each in set_cookies:
-        if "BAIDUID" in each:
-            cookie = each
-            break
-    #必须要重新访问获取token
-    if token == None:
-         tokenHeaders = {}
-         tokenHeaders["Cookie"] = cookie
-         tokenRequest = request.Request("http://fanyi.baidu.com/?aldtype=16047",None,tokenHeaders)
-         with request.urlopen(tokenRequest) as tokenResponse:
-             infoStr = tokenResponse.read().decode("utf-8")
-             tokenRe = re.search(r"token: '([\w]+)',",infoStr)
-             token = tokenRe.group(1)
-    fromLan = _findType(query)
-    url="http://fanyi.baidu.com/v2transapi"
-    
+baidu_sign = baidu_token = baidu_set_cookies = None
+
+# 查询翻译结果
+def findTranslateResult(query, from_lang, to_lang):
+    global baidu_sign, baidu_token, baidu_set_cookies
+    if baidu_set_cookies == None or baidu_sign == None:
+        gtkResponse = urllib2.urlopen("http://fanyi.baidu.com/?aldtype=16047")
+        baidu_set_cookies = gtkResponse.info().get("Set-Cookie")
+        infoStr = gtkResponse.read().decode("utf-8")
+        gtkRe = re.search(r";window.gtk = '([\d\.]+)';", infoStr)
+        gtk = gtkRe.group(1)
+        baidu_sign = _calcSign(query, gtk)
+    cookie = baidu_set_cookies.split('domain=.baidu.com,')[1]
+
+    # 必须要重新访问获取token
+    if baidu_token == None:
+        tokenHeaders = {}
+        tokenHeaders["Cookie"] = cookie
+        tokenRequest = urllib2.Request("http://fanyi.baidu.com/?aldtype=16047", None, tokenHeaders)
+        tokenResponse = urllib2.urlopen(tokenRequest)
+        infoStr = tokenResponse.read().decode("utf-8")
+        tokenRe = re.search(r"token: '([\w]+)',", infoStr)
+        baidu_token = tokenRe.group(1)
+    url = "https://fanyi.baidu.com/v2transapi"
+
     headers = {}
     headers["Cookie"] = cookie
     data = {}
-    data["from"] = fromLan
-    data["to"] = "en"
+    data["from"] = from_lang
+    data["to"] = to_lang
     data["query"] = query
-    data["transtype"] = "translang"
+    data["transtype"] = "realtime"
     data["simple_means_flag"] = "3"
-    data["sign"] = sign
-    token = token
-    data["token"] = token
-    data = parse.urlencode(data).encode("utf-8")
-    req = request.Request(url,data,headers)
-    result = None
-    with request.urlopen(req) as response:
-        result = response.read().decode("utf-8")
-    jsonResult = json.loads(result)
-    jsonResult = jsonResult["dict_result"]["simple_means"]["symbols"][0]["parts"][0]["means"]
-    resultList = [];
-    for each in jsonResult:
-        resultList.append(each["text"])
-    return resultList
+    data["sign"] = baidu_sign
+    data["token"] = baidu_token
+    data = urllib.urlencode(data).encode("utf-8")
+    req = urllib2.Request(url = url, data = data, headers = headers)
+    response = urllib2.urlopen(req)
+    all_text = response.read().decode("utf-8")
+    jsonResult = json.loads(all_text)
+    result = jsonResult['trans_result']['data'][0]['dst']
+    return result
 
-#计算sign
-def _calcSign(query,gtk):
-    with open("sign.js","r",encoding="utf-8") as f:
-        jsStr  = f.read()
+
+# 计算sign
+def _calcSign(query, gtk):
+    with open("sign.js", "r") as f:
+        jsStr = f.read()
     ctx = execjs.compile(jsStr)
-    result = ctx.call("e",query,gtk)
-    return result;
-    
-if __name__ == "__main__":
-    print(findTranslateResult("推送"))
-       
-        
+    result = ctx.call("e", query, gtk)
+    return result
 
-    
+
+if __name__ == "__main__":
+    print(findTranslateResult("hello",'en','spa'))
